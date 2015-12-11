4d9a6a61b7cb438fa5ddc948ca19de0ba532fa40
Kazoldrek <mathmontb@hotmail.com>
2015-12-11 11:50:53 +0100

    ajout commentaires

--- src/finalStep/Marcourt.scala
+++ src/finalStep/Marcourt.scala
@@ -7,6 +7,12 @@ object Marcourt extends jacop {
 
   def main(args: Array[String]): Unit = {
 
+    /**
+     * construit la liste des cours sur base de deux fichiers de configuration
+     * @param fichierCours le nom du fichier de configuration des cours
+     * @param fichierEleve le nom du fichier de configuration de l eleve
+     * @return la liste de cours
+     */
     def creerCours(fichierCours: String, fichierEleve: String): List[Cours] = {
       def creerCour(itCours: Iterator[String], itEleve: Iterator[String]): List[Cours] = {
         if (itCours.hasNext && itEleve.hasNext) {
@@ -24,6 +30,10 @@ object Marcourt extends jacop {
       creerCour(itCours, itEleve)
     }
 
+    /**
+     * Pose Les conditions nécessaires sur les corequis
+     * @param toutCorequis est la liste de tout les corequis
+     */
     def creerCorequis(toutCorequis: List[List[Cours]]) {
       for (listeCorequis <- toutCorequis) {
         for (i <- 0 until listeCorequis.length - 1) {
@@ -33,6 +43,11 @@ object Marcourt extends jacop {
       }
     }
 
+    /**
+     * Pose les conditions nécessaires sur les corequis
+     * @param toutPrerequis la liste de tout les prerequis
+     * @return une liste d IntVar permettant de savoir si un prérequis a été converti en corequis
+     */
     def creerPrerequis(toutPrerequis: List[List[Cours]]): List[IntVar] = {
       val listConversion = for (prerequis <- toutPrerequis) yield IntVar("", 0, 1)
 
@@ -47,6 +62,12 @@ object Marcourt extends jacop {
       listConversion
     }
 
+    /**
+     * Cree une liste d'IntVar en en collant deux
+     * @param x une liste
+     * @param y une liste
+     * @return la liste créée
+     */
     def append(x: List[IntVar], y: List[IntVar]): List[IntVar] = {
       x match {
         case Nil => y match {
@@ -57,33 +78,50 @@ object Marcourt extends jacop {
       }
     }
 
+    /**
+     * Pose les conditions nécessaires pour les cours blocants
+     * @param cours la liste de tout les cours
+     * @return une liste d IntVar permettants de savoir si une contrainte de cours bloquant a été transgressée
+     */
     def creerCoursBloquant(cours: List[Cours]): List[IntVar] = {
+      def creerContraintesCoursBloquants(cours: List[Cours]): List[IntVar] = {
+
+        cours match {
+          case Nil => Nil
+          case tete :: queue => {
+            val intvars = for (i <- List.range(1, cours.length)) yield new IntVar("", 0, 1)
+            for (i <- 1 until cours.length) {
+              (tete.booleen /\ cours(i).booleen) <=> (intvars(i - 1) #= 1)
+            }
+            append(creerContraintesCoursBloquants(queue), intvars)
+          }
+        }
+      }
+
       val liste1 = for (c <- cours if c.blocant == 1) yield c
       val liste2 = for (c <- cours if c.blocant == 2) yield c
 
       append(creerContraintesCoursBloquants(liste1), creerContraintesCoursBloquants(liste2))
     }
 
-    def creerContraintesCoursBloquants(cours: List[Cours]): List[IntVar] = {
-
-      cours match {
-        case Nil => Nil
-        case tete :: queue => {
-          val intvars = for (i <- List.range(1, cours.length)) yield new IntVar("", 0, 1)
-          for (i <- 1 until cours.length) {
-            (tete.booleen /\ cours(i).booleen) <=> (intvars(i - 1) #= 1)
-          }
-          append(creerContraintesCoursBloquants(queue), intvars)
-        }
-      }
-    }
-
+    /**
+     * Crée les listes formatées correctement pour les corequis ou prerequis
+     * @param fichier le nom du fichier a traiter
+     * @param cours la liste entiere des cours
+     * @return une liste de corequis ou prerequis formatée correctement
+     */
     def lireCoPreRequis(fichier: String, cours: List[Cours]): List[List[Cours]] = {
       val t = Source.fromFile(fichier).getLines.filterNot(_.startsWith("#"))
       val list = (for (a <- t) yield List(a.split(";")(0).toInt - 1, a.split(";")(1).toInt - 1)).toList
       for (a <- list) yield List(cours(a(0)), cours(a(1)))
     }
 
+    /**
+     * Additionne un int et les credits d un cours
+     * @param x un int
+     * @param y un cours
+     * @return la somme de l int et des crédits du cours
+     */
     def addEcts(x: Int, y: Cours): Int = x + y.ects
 
     def sommeCoursEctsRestants(cours: List[Cours]): Int = {
@@ -110,7 +148,6 @@ object Marcourt extends jacop {
 
     //creation des objets Cours et les mettre dans une liste
     val cours = creerCours("cours.txt", "eleve.txt")
-    println(cours)
     val coursBloc1 = for (a <- cours if a.blocCours == 1) yield a
 
     val prerequis = lireCoPreRequis("prerequis.txt", cours)
